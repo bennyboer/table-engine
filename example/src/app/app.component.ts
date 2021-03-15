c127ec5da85d643b25c9032f75407ad41c9a00ea
bennyboer <barny.eder@gmail.com>
2021-03-15 20:50:30 +0100

    Working CanvasKit example

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,15 +1,19 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, ViewChild} from '@angular/core';
 import {TableEngine} from "../../../src/main";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
 
+  @ViewChild("canvas")
+  public canvasElement!: ElementRef;
+
   public test(): void {
-    new TableEngine();
+    new TableEngine(this.canvasElement.nativeElement as HTMLCanvasElement);
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,40 @@
 import {InlineWorker} from "./util/worker/inline-worker";
+import CanvasKitInit, {CanvasKit, Surface} from "canvaskit-wasm";
 
 /**
  * Entry point of the table engine library.
  */
 export class TableEngine {
-	constructor() {
+	constructor(canvasElement: HTMLCanvasElement) {
 		const worker: TestWorker = new TestWorker();
 
 		worker.postMessage("What are you doing?");
+
+		(CanvasKitInit as any)({
+			locateFile: (file: string) => 'https://unpkg.com/canvaskit-wasm@0.25.0/bin/' + file,
+		}).then((canvasKit: CanvasKit) => {
+			const surface: any = canvasKit.MakeCanvasSurface(canvasElement);
+
+			const paint = new canvasKit.Paint();
+			paint.setColor(canvasKit.Color4f(0.9, 0, 0, 1.0));
+			paint.setStyle(canvasKit.PaintStyle.Stroke);
+			paint.setAntiAlias(true);
+			const rr = canvasKit.RRectXY(canvasKit.LTRBRect(10, 60, 210, 260), 25, 15);
+
+			function draw(canvas: any) {
+				canvas.clear(canvasKit.WHITE);
+				canvas.drawRRect(rr, paint);
+			}
+
+			surface.drawOnce(draw);
+		});
 	}
 }
 
-export class TestWorker extends InlineWorker<string>{
+export class TestWorker extends InlineWorker<string> {
 
 	constructor() {
-		super(function(this: any) {
+		super(function (this: any) {
 			const self = this;
 
 			self.onmessage = (message: MessageEvent<string>) => {
