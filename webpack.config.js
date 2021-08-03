c3eadd7390441e41b18fc66ccc1195cb011de303
bennyboer <barny.eder@gmail.com>
2021-08-03 20:54:50 +0200

    Adding webpack build

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+	mode: "production",
+	entry: "./src/table-engine.ts",
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "lib/bundled"),
+	},
+};
